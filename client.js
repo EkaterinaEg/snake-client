a66a0295beb4a769e2b4b1b18525b40784cbbe90
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-10-23 16:48:57 -0400

    add Movements

--- client.js
+++ client.js
@@ -5,17 +5,45 @@ const connect = function() {
     host: "165.227.47.243",
     port: 50541,
   });
-  // const conn = net.createConnection();
 
   conn.on("data", (data) => {
     console.log(data);
   });
+
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
   });
+
+  //Send name to server
   conn.on("connect", () => {
     conn.write("Name: EEg");
   });
+
+  //Movements
+  conn.on("connect", () => {
+    //setTimeout
+    // _____________________
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 50);
+    // setTimeout(() => {
+    //   conn.write("Move: down");
+    // }, 50);
+    // setTimeout(() => {
+    //   conn.write("Move: left");
+    // }, 50);
+    ///setInterval
+    // ________________________
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 1000);
+    // -------------------------------------
+    // conn.write("Move: up");
+    // conn.write("Move: down");
+    // conn.write("Move: left");
+    // conn.write("Move: right");
+  });
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
