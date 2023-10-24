167b536235373a6f101b79dd097a9cea9713831e
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-10-23 21:23:40 -0400

    add constants

---
+++ constants.js
@@ -0,0 +1,15 @@
+const IP = "165.227.47.243";
+const PORT = 50541;
+
+const KEYS = {
+  w: "Move: up", //w
+  q: "Move: left", //q
+  s: "Move: down", //s
+  d: "Move: right", //d
+};
+
+module.exports = {
+  IP,
+  PORT,
+  KEYS,
+};
--- input.js
+++ input.js
@@ -1,8 +1,9 @@
 const stdin = process.stdin;
+const { KEYS } = require("./constants");
 
 let connection;
 
-const setupInput = function (conn) {
+const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
@@ -11,37 +12,41 @@ const setupInput = function (conn) {
   return stdin;
 };
 
-const handleUserInput = function (key) {
+const handleUserInput = function(key) {
   if (key === "\u0003") {
     process.exit();
   }
-  if (key === "\u0077") {
+  // for (const key in KEYS) {
+  //   connection.write(KEYS[key]);
+  // }
+
+  if (key === "h") {
+    //h => How are you!
+    connection.write("Say: HAY");
+  }
+  if (key === "e") {
+    //e => AiH!
+    connection.write("Say: WIN");
+  }
+  if (key === "w") {
     connection.write("Move: up");
     //w
     // console.log("Move: up");
   }
-  if (key === "\u0061") {
+  if (key === "a") {
     connection.write("Move: left");
     //a
     // console.log("Move: left");
   }
-  if (key === "\u0073") {
+  if (key === "s") {
     connection.write("Move: down");
     //s
     // console.log("Move: down");
   }
-  if (key === "\u0064") {
+  if (key === "d") {
     connection.write("Move: right");
     //d
     // console.log("Move: right");
-  }
-  if (key === "\u0068") {
-    //h => How are you!
-    connection.write("HAY");
-  }
-  if (key === "\u0065") {
-    //e => AiH!
-    connection.write("AiH");
   }
 };
 
